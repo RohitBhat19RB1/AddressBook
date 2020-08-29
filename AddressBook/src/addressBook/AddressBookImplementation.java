b97f6a4aa54cc496f6fcb79f0e81846f0fecfffc
RohitBhat19RB1 <rohitbhat16860@gmail.com>
2020-08-29 11:49:34 +0530

    Added:editPerson in openAddressBook.It helps to edit person details.

--- AddressBook/src/addressBook/AddressBookImplementation.java
+++ AddressBook/src/addressBook/AddressBookImplementation.java
@@ -1,105 +1,193 @@
 package addressBook;
-import java.io.*;
-import java.util.*;
 
-
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookImplementation implements AddressBookInterface {
-ArrayList<Person> persons;
-	
-	public AddressBookImplementation () {
+	ArrayList<Person> persons;
+
+	public AddressBookImplementation() {
 		persons = new ArrayList<Person>();
 	}
-	
-	public void addPerson(String n){
-		
+
+	public void addPerson(String n) {
+
 		{
 			try {
-					 System.out.println("how many enteries u want to enter = ");
-					 @SuppressWarnings("resource")
-					 Scanner sc = new Scanner(System.in);
-						int entry = sc.nextInt();
-				
-						 File myObj = new File("C:\\AddressBook\\"+n);
-						FileWriter fw = new FileWriter(myObj, true);
-				      PrintWriter pw = new PrintWriter(fw, true);
-				    { for(int i=0; i < entry; i++) 
-				        {	    System.out.println("for entry "+ (i+1));
-						    	System.out.println("Enter first name = ");
-							    String firstName = sc.next();
-							    System.out.println("Enter last name = ");
-							    String lastName = sc.next();
-							    System.out.println("Enter city = ");
-							    String city = sc.next();
-							    System.out.println("Enter state = ");
-								String state = sc.next();
-								System.out.println("Enter zipcode = ");
-								long zipCode = sc.nextInt();
-								System.out.println("Enter phone number = ");
-								long pNum = sc.nextLong();
-								//construct new person object
-								Person p = new Person(firstName, lastName, city, state, zipCode, pNum);
-								//add the above PersonInfo object to arraylist
-								persons.add(p);
-				            
-								fw.append(p.getfirstName()+" ");
-								fw.append(p.getlastName()+" ");
-								fw.append(p.getcity()+" ");
-								fw.append(p.getstate()+" ");
-								pw.print(p.getzipCode()+" ");
-								pw.print(p.getpNum()+" ");
-				        }
+				System.out.println("how many enteries u want to enter = ");
+				@SuppressWarnings("resource")
+				Scanner sc = new Scanner(System.in);
+				int entry = sc.nextInt();
+
+				File myObj = new File("C:\\AddressBook\\" + n);
+				FileWriter fw = new FileWriter(myObj, true);
+				PrintWriter pw = new PrintWriter(fw, true);
+				{
+					for (int i = 0; i < entry; i++) {
+						System.out.println("for entry " + (i + 1));
+						System.out.println("Enter first name = ");
+						String firstName = sc.next();
+						System.out.println("Enter last name = ");
+						String lastName = sc.next();
+						System.out.println("Enter city = ");
+						String city = sc.next();
+						System.out.println("Enter state = ");
+						String state = sc.next();
+						System.out.println("Enter zipcode = ");
+						String zipCode = sc.next();
+						System.out.println("Enter phone number = ");
+						String pNum = sc.next();
+						// construct new person object
+						Person p = new Person(firstName, lastName, city, state, zipCode, pNum);
+						// add the above PersonInfo object to arraylist
+						persons.add(p);
+
+						fw.append(p.getfirstName() + " ");
+						fw.append(p.getlastName() + " ");
+						fw.append(p.getcity() + " ");
+						fw.append(p.getstate() + " ");
+						pw.print(p.getzipCode() + " ");
+						pw.print(p.getpNum() + " ");
+					}
 					fw.close();
 					pw.close();
-				    }
-				      System.out.println("Successfully wrote to the file");
-				      System.out.println("After modifications ");
-				      try {
-					      File myOb = new File("C:\\AddressBook\\"+n);
-					      Scanner myReader = new Scanner(myOb);
-					      while (myReader.hasNextLine()) {
-					        String data = myReader.nextLine();
-					        System.out.println(data);
-					      }
-					      myReader.close();
-					    } catch (FileNotFoundException e) {
-					      System.out.println("An error occurred.");
-					      e.printStackTrace();
-					    }
-
-			  } catch (IOException e) {
-				      System.out.println("An error occurred.");
-				      e.printStackTrace();
-	       }		 
-		} 
-	
-		
-	
-	}
-	
-	public void editPerson() {	
-		
-		}
-	
-	public void deletePerson() {
-		
-	}
-	
-	public void searchPerson() {
-		 
-	} 
-	
-	public void sortByZip() {
-		
-	}
-	
-	public void sortByNames() {
-		
 				}
-	
-	public void display() {
-		 
-		
+				System.out.println("Successfully wrote to the file");
+				System.out.println("After modifications ");
+				try {
+					File myOb = new File("C:\\AddressBook\\" + n);
+					Scanner myReader = new Scanner(myOb);
+					while (myReader.hasNextLine()) {
+						String data = myReader.nextLine();
+						System.out.println(data);
+					}
+					myReader.close();
+				} catch (FileNotFoundException e) {
+					System.out.println("An error occurred.");
+					e.printStackTrace();
+				}
+
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		}
+
 	}
-	
-}	
+
+	public void editPerson(String n, String num) {
+		@SuppressWarnings("resource")
+		Scanner sca = new Scanner(System.in);
+		System.out.println("Enter the new first name = ");
+		String nfName = sca.next();
+		System.out.println("Enter new last name = ");
+		String nlName = sca.next();
+		System.out.println("Enter city = ");
+		String ncity = sca.next();
+		System.out.println("Enter state = ");
+		String nstate = sca.next();
+		System.out.println("Enter zipcode = ");
+		String nzipCode = sca.next();
+		System.out.println("Enter phone number = ");
+		String npNum = sca.next();
+		File oldFile = new File("C:\\AddressBook\\" + n);
+		File newFile = new File("C:\\AddressBook\\temp.txt");
+		String firstName = "";
+		String lastName = "";
+		String city = "";
+		String state = "";
+		String zipCode = "";
+		String pNum = "";
+		Scanner x;
+		try {
+			FileWriter fw = new FileWriter("C:\\AddressBook\\temp.txt", true);
+			PrintWriter pw = new PrintWriter(fw);
+			x = new Scanner(new File("C:\\AddressBook\\" + n));
+			x.useDelimiter("[ \n]");
+			while (x.hasNext()) {
+				firstName = x.next();
+				lastName = x.next();
+				city = x.next();
+				state = x.next();
+				zipCode = x.next();
+				pNum = x.next();
+				if (pNum.equals(num)) {
+					// construct new person object
+					Person p = new Person(nfName, nlName, ncity, nstate, nzipCode, npNum);
+					// add the above PersonInfo object to arraylist
+					persons.add(p);
+					pw.println(p.getfirstName() + " ");
+					pw.println(p.getlastName() + " ");
+					pw.println(p.getcity() + " ");
+					pw.println(p.getstate() + " ");
+					pw.println(p.getzipCode() + " ");
+					pw.println(p.getpNum() + " ");
+				} else {
+					Person p = new Person(firstName, lastName, city, state, zipCode, pNum);
+					persons.add(p);
+					pw.println(p.getfirstName() + " ");
+					pw.println(p.getlastName() + " ");
+					pw.println(p.getcity() + " ");
+					pw.println(p.getstate() + " ");
+					pw.println(p.getzipCode() + " ");
+					pw.println(p.getpNum() + " ");
+				}
+			}
+			x.close();
+			pw.close();
+			newFile.delete();
+			File dump = new File("C:\\AddressBook\\" + n);
+			oldFile.renameTo(dump);
+			System.out.println("Successfully wrote to the file");
+
+		} catch (Exception e) {
+			System.out.println("error");
+		}
+		{
+			try {
+				File myObj = new File("C:\\AddressBook\\" + n);
+				Scanner myReader = new Scanner(myObj);
+				while (myReader.hasNextLine()) {
+					String data = myReader.nextLine();
+					System.out.println(data);
+				}
+				myReader.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void deletePerson() {
+
+	}
+
+	public void searchPerson(String n) {
+
+	}
+
+	public void sortByZip() {
+
+	}
+
+	public void sortByNames() {
+
+	}
+
+	public void display() {
+
+	}
+
+	@Override
+	public void editPerson() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- AddressBook/src/addressBook/AddressBookManagerImplementation.java
+++ AddressBook/src/addressBook/AddressBookManagerImplementation.java
@@ -1,173 +1,179 @@
 package addressBook;
-import java.io.*;
+
+import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.*;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class AddressBookManagerImplementation implements AddressBookManagerInterface {
 	static AddressBookImplementation ab = new AddressBookImplementation();
 	String s;
 	ArrayList<Person> persons;
-	
-	public AddressBookManagerImplementation () 	{
+
+	public AddressBookManagerImplementation() {
 		persons = new ArrayList<Person>();
 	}
+
 	@SuppressWarnings("resource")
-		@Override
-	public void newAddressBook() {
-			
-			Scanner sc = new Scanner(System.in);
-			System.out.println("By What name would you like to save ur new Address Book ---- ");
-			String name = sc.nextLine();
-			System.out.println("what extension would you like to give (.csv , .txt or .json) ---- ");
-			String ext = sc.nextLine();
-
-		{ try 
-			   {
-				 File myObj = new File("C:\\AddressBook\\"+name+ext);
-			         if (myObj.createNewFile()) 
-			         	{
-			    	      System.out.println("File created: " + myObj.getName());
-		         	    }
-			         else 
-			      		{
-			        	 System.out.println("File already exists.");
-			      		}
-			    } catch (IOException e) {
-			      System.out.println("An error occurred.");
-			      e.printStackTrace();
-			    						}
-		}	
-				
-			 {
-			try {
-					 System.out.println("how many enteries u want to enter = ");
-					 	int entry = sc.nextInt();
-				        FileWriter myWriter = new FileWriter("C:\\AddressBook\\"+name+ext);
-				        PrintWriter pw = new PrintWriter(myWriter);
-				    { for(int i=0; i < entry; i++) 
-				        {	    System.out.println("for entry "+ (i+1));
-						    	System.out.println("Enter first name = ");
-							    String firstName = sc.next();
-							    System.out.println("Enter last name = ");
-							    String lastName = sc.next();
-							    System.out.println("Enter city = ");
-							    String city = sc.next();
-							    System.out.println("Enter state = ");
-								String state = sc.next();
-								System.out.println("Enter zipcode = ");
-								long zipCode = sc.nextLong();
-								System.out.println("Enter phone number = ");
-								long pNum = sc.nextLong();
-								//construct new person object
-								Person p = new Person(firstName, lastName, city, state, zipCode, pNum);
-								//add the above PersonInfo object to arraylist
-								persons.add(p);
-				            
-							 pw.print(p.getfirstName()+" ");
-				             pw.print(p.getlastName()+" ");
-				             pw.print(p.getcity()+" ");
-				             pw.print(p.getstate()+" ");
-				             pw.print(p.getzipCode()+" ");
-				             pw.print(p.getpNum());
-				             pw.println();
-				        }
-				      pw.close();
-				    }
-				      System.out.println("Successfully wrote to the file");
-
-			  } catch (IOException e) {
-				      System.out.println("An error occurred.");
-				      e.printStackTrace();
-	       }		 
-		} 
-
-								
-	
-	}
 	@Override
-	public void openAddressBook()  {
+	public void newAddressBook() {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("By What name would you like to save ur new Address Book ---- ");
+		String name = sc.nextLine();
+		System.out.println("what extension would you like to give (.csv , .txt or .json) ---- ");
+		String ext = sc.nextLine();
+
+		{
+			try {
+				File myObj = new File("C:\\AddressBook\\" + name + ext);
+				if (myObj.createNewFile()) {
+					System.out.println("File created: " + myObj.getName());
+				} else {
+					System.out.println("File already exists.");
+				}
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		}
+
+		{
+			try {
+				System.out.println("how many enteries u want to enter = ");
+				int entry = sc.nextInt();
+				FileWriter myWriter = new FileWriter("C:\\AddressBook\\" + name + ext);
+				PrintWriter pw = new PrintWriter(myWriter);
+				{
+					for (int i = 0; i < entry; i++) {
+						System.out.println("for entry " + (i + 1));
+						System.out.println("Enter first name = ");
+						String firstName = sc.next();
+						System.out.println("Enter last name = ");
+						String lastName = sc.next();
+						System.out.println("Enter city = ");
+						String city = sc.next();
+						System.out.println("Enter state = ");
+						String state = sc.next();
+						System.out.println("Enter zipcode = ");
+						String zipCode = sc.next();
+						System.out.println("Enter phone number = ");
+						String pNum = sc.next();
+						// construct new person object
+						Person p = new Person(firstName, lastName, city, state, zipCode, pNum);
+						// add the above PersonInfo object to arraylist
+						persons.add(p);
+
+						pw.print(p.getfirstName() + " ");
+						pw.print(p.getlastName() + " ");
+						pw.print(p.getcity() + " ");
+						pw.print(p.getstate() + " ");
+						pw.print(p.getzipCode() + " ");
+						pw.print(p.getpNum());
+						pw.println();
+					}
+					pw.close();
+				}
+				System.out.println("Successfully wrote to the file");
+
+			} catch (IOException e) {
+				System.out.println("An error occurred.");
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	@Override
+	public void openAddressBook() {
 		System.out.println("Total AddressBooks present  ");
-  	  File file = new File("C:\\AddressBook\\");
-  	  String[] files=file.list();
-  	   for (String string : files) {
-  	  System.out.println(string);
-  	   }
+		File file = new File("C:\\AddressBook\\");
+		String[] files = file.list();
+		for (String string : files) {
+			System.out.println(string);
+		}
 		System.out.println("Enter the name of Address Book you want to open --- ");
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		String name1 = sc.next();
-		 try {
-		      File myObj = new File("C:\\AddressBook\\"+name1);
-		      Scanner myReader = new Scanner(myObj);
-		      while (myReader.hasNextLine()) {
-		        String data = myReader.nextLine();
-		        System.out.println(data);
-		      }
-		      myReader.close();
-		    } catch (FileNotFoundException e) {
-		      System.out.println("An error occurred.");
-		      e.printStackTrace();
-		    }
-		 
-		 	System.out.println("Please Choose from the below given Options what Operations would you like to perform");
-		 	System.out.println();
-		 	System.out.println("\t Enter (1) to add new Person");
-			System.out.println("\t Enter (2) to edit any Person");
-			System.out.println("\t Enter (3) to delete any Person");
-			System.out.println("\t Enter (4) to search Person");
-			System.out.println("\t Enter (5) to sort by Zip");
-			System.out.println("\t Enter (6) to sort by name");
-			System.out.println("\t Enter (7) to display");
-			System.out.println();
-			
-			try (Scanner sci = new Scanner(System.in)){
-				System.out.print("Please Enter your choice  --->");
-				int ch = sci.nextInt();
-				
-				switch(ch) {
-				case 1:
+		try {
+			File myObj = new File("C:\\AddressBook\\" + name1);
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
+
+		System.out.println("Please Choose from the below given Options what Operations would you like to perform");
+		System.out.println();
+		System.out.println("\t Enter (1) to add new Person");
+		System.out.println("\t Enter (2) to edit any Person");
+		System.out.println("\t Enter (3) to delete any Person");
+		System.out.println("\t Enter (4) to search Person");
+		System.out.println("\t Enter (5) to sort by Zip");
+		System.out.println("\t Enter (6) to sort by name");
+		System.out.println("\t Enter (7) to display");
+		System.out.println();
+
+		try (Scanner sci = new Scanner(System.in)) {
+			System.out.print("Please Enter your choice  --->");
+			int ch = sci.nextInt();
+			switch (ch) {
+			case 1:
 				ab.addPerson(name1);
-					break;
-				case 2:
-				ab.editPerson();
 				break;
-				case 3:
+			case 2:
+				System.out.println("Enter the phone number of the person u want to edit-");
+				String number = sci.next();
+				ab.editPerson(name1, number);
+				break;
+			case 3:
 				ab.deletePerson();
 				break;
-				case 4:
-				ab.searchPerson();
+			case 4:
+				ab.searchPerson(name1);
 				break;
-				case 5:
+			case 5:
 				ab.sortByZip();
 				break;
-				case 6:
+			case 6:
 				ab.sortByNames();
 				break;
-				case 7:
+			case 7:
 				ab.display();
-				break;	
-				default:
-					System.out.println("please enter your choice from the menu");
-				}
-				}
-		
+				break;
+			default:
+				System.out.println("please enter your choice from the menu");
+			}
+		}
+
 	}
+
 	@Override
 	public void saveAddressBook() {
-		
+
 	}
+
 	@Override
 	public void saveAsAddressBook() {
-	   	
+
 	}
+
 	@Override
 	public void closeAddressBook() {
-		
+
 	}
+
 	@Override
 	public void quit() {
-		
+
 	}
-	
-	
 }
